9443d6177676113d336b585ec2865af9bcb31fca
Tobias Roth <tobias@Mac-Tobi.local>
2018-11-04 23:28:59 +0100

    First analyses.

---
+++ R/Auswertungen.R
@@ -0,0 +1,52 @@
+rm(list=ls(all=TRUE))
+
+#------------------------------------------------------------------------------
+# Settings
+#------------------------------------------------------------------------------
+# Libraries
+library(tidyverse)
+library(readxl)
+
+#------------------------------------------------------------------------------
+# Load and prepare data
+#------------------------------------------------------------------------------
+# Load data
+d <- read_excel("Data/Data_combined.xlsx") 
+
+# Correct data
+d$Object_Type[d$Object_Type == "blank" & !is.na(d$Object_Type)] <- "Blank"
+
+# Aggregate data
+dat <- d %>% 
+  group_by(Day, Species) %>% 
+  summarise(n_obs_tot = n(),
+            Blank = sum(Object_Type == "Blank", na.rm = TRUE),
+            Human = sum(Object_Type == "Human", na.rm = TRUE),
+            Mirror = sum(Object_Type == "Mirror", na.rm = TRUE),
+            Monkey = sum(Object_Type == "Monkey", na.rm = TRUE)) %>% 
+  gather("Device", "nobs", Blank, Human, Mirror, Monkey)
+
+#------------------------------------------------------------------------------
+# Some descriptive statistics
+#------------------------------------------------------------------------------
+table(dat$Species)
+
+#------------------------------------------------------------------------------
+# Make a figure / test per species 
+#------------------------------------------------------------------------------
+spec = "Ateles"
+spec = "Macaca"
+ggplot(dat %>% filter(Species == spec), 
+       aes(x = Day, y = nobs / n_obs_tot, col = Device)) +
+  geom_line() +
+  geom_point() + 
+  labs(y = "Proportion of times the device was used",
+       title = paste(spec))
+  
+# Test per species
+mod1 <- glm(cbind(nobs, n_obs_tot-nobs) ~ Device * Day, family = binomial,
+    data = dat %>% filter(Species == spec)) 
+mod2 <- glm(cbind(nobs, n_obs_tot-nobs) ~ Device + Day, family = binomial,
+            data = dat %>% filter(Species == spec)) 
+anova(mod1, mod2, test = "Chisq")
+summary(mod2)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# master-mirrors
+# Experiment with mirrors in Zooli
 Are Mirrors Suitable Environmental Enrichment Devices Regarding Different Primate Species? Analyses for the master thesis of Natalie Jeanneret. 
+
+### Description of data
+- The data are contained in the folder `Data`.
+- The R-script "Auswertung.R" contains the analyses.
